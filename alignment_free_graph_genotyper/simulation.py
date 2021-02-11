b2c3395455a987ea6737d7a4b2be1754c2c6b10f
Ivar Grytten <ivar.grytten@gmail.com>
2021-02-11 15:35:17 +0100

    Rewrote simulation

--- alignment_free_graph_genotyper/simulation.py
+++ alignment_free_graph_genotyper/simulation.py
@@ -4,6 +4,7 @@ from obgraph import GenotypeFrequencies, MostSimilarVariantLookup
 from .node_count_model import NodeCountModel
 import numpy as np
 from .variants import VcfVariant, VcfVariants
+from collections import defaultdict
 
 
 def run_genotyper_on_simualated_data(genotyper, n_variants, average_coverage, coverage_std):
@@ -11,7 +12,8 @@ def run_genotyper_on_simualated_data(genotyper, n_variants, average_coverage, co
     variants, node_counts, model, genotype_frequencies, most_similar_variant_lookup, variant_to_nodes = simulator.run()
     print(model.node_counts_following_node)
     print(model.node_counts_not_following_node)
-
+    print(node_counts.node_counts)
+    print(variants)
     truth_variants = variants.copy()
 
     g = genotyper(model, variants, variant_to_nodes, node_counts, genotype_frequencies, most_similar_variant_lookup)
@@ -58,6 +60,7 @@ class GenotypingDataSimulator:
             variant = VcfVariant(1, i, "A", "T", type="SNP")
             genotype = np.random.choice(possible_genotypes)
             variant.set_genotype(genotype)
+            print(variant)
 
             ref_count = 0
             var_count = 0
@@ -70,34 +73,47 @@ class GenotypingDataSimulator:
                 else:
                     ref_count += real_reads_on_haplotype
 
-                self._expected_counts_following_node[self._reference_nodes[i]] += self._average_coverage
-                self._expected_counts_following_node[self._variant_nodes[i]] += self._average_coverage
-
-                if np.random.random() < 0.1:
-                    n_duplicate_areas = np.random.randint(0, 3)
-                    for duplicate_area in range(n_duplicate_areas):
-                        reads_from_duplicate = int(np.random.normal(self._average_coverage, self._coverage_std))
-                        # 50/50 chance that this duplicate follows the node or not
-                        follows_node = False
-                        if np.random.randint(0, 2) == 1:
-                            follows_node = True
-
-                        if haplotype == "1":
-                            var_count += reads_from_duplicate
-                            if follows_node:
-                                self._expected_counts_following_node[self._variant_nodes[i]] += self._average_coverage
-                            else:
-                                self._expected_counts_not_following_node[self._variant_nodes[i]] += self._average_coverage
-                        else:
-                            ref_count += reads_from_duplicate
-                            if follows_node:
-                                self._expected_counts_following_node[self._reference_nodes[i]] += self._average_coverage
-                            else:
-                                self._expected_counts_not_following_node[self._reference_nodes[i]] += self._average_coverage
-
-
-            variants.append(variant)
             self._node_counts[self._variant_nodes[i]] += var_count
             self._node_counts[self._reference_nodes[i]] += ref_count
+
+            variants.append(variant)
+
+
+        # Duplications
+        n_duplications_per_variant_ref = defaultdict(int)
+        n_duplications_per_variant_variant = defaultdict(int)
+
+        for i in range(self._n_variants):
+            if np.random.random() < 0.1:
+                n_duplicate_areas = np.random.randint(0, 3)
+                for duplicate_area in range(n_duplicate_areas):
+                    reads_from_duplicate = int(np.random.normal(self._average_coverage, self._coverage_std))
+                    # 50/50 chance that this duplicate is on variant or ref node
+                    if np.random.random() < 0.5:
+                        self._expected_counts_following_node[self._variant_nodes[i]] += self._average_coverage
+                        self._expected_counts_not_following_node[self._variant_nodes[i]] += self._average_coverage
+                        n_duplications_per_variant_variant[i] += 1
+                    else:
+                        self._expected_counts_following_node[self._reference_nodes[i]] += self._average_coverage
+                        self._expected_counts_not_following_node[self._reference_nodes[i]] += self._average_coverage
+                        n_duplications_per_variant_ref[i] += 1
+
+
+        # Add observed counts from duplicates
+        for variant_id, n_duplications in n_duplications_per_variant_ref.items():
+            for i in range(n_duplications):
+                self._node_counts[self._reference_nodes[variant_id]] += np.random.normal(self._average_coverage, self._coverage_std)
+
+        for variant_id, n_duplications in n_duplications_per_variant_variant.items():
+            for i in range(n_duplications):
+                self._node_counts[self._variant_nodes[variant_id]] += np.random.normal(self._average_coverage,
+                                                                                         self._coverage_std)
+
+        # non-duplicate counts, from actual variant
+        for i in range(self._n_variants):
+            self._expected_counts_following_node[self._reference_nodes[i]] += self._average_coverage
+            self._expected_counts_following_node[self._variant_nodes[i]] += self._average_coverage
+
+
 
         self._variants = VcfVariants(variants)
