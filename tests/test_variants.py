1f8d8109eae28c58ca23c5f09e49f9182a77417a
Ivar Grytten <ivar.grytten@gmail.com>
2021-05-05 14:38:38 +0200

    Tests

---
+++ tests/test_variants.py
@@ -0,0 +1,22 @@
+from alignment_free_graph_genotyper.variants import VcfVariant, VcfVariants
+
+def test_position_index():
+    variants = VcfVariants(
+        [
+            VcfVariant(1, 4),
+            VcfVariant(1, 7),
+            VcfVariant(1, 8),
+            VcfVariant(1, 8),
+            VcfVariant(3, 8),
+            VcfVariant(3, 9),
+            VcfVariant(3, 11),
+        ]
+    )
+
+    variants.make_position_index()
+
+    v = variants.get_variants_in_region(1, 4, 8)
+    print(v)
+
+
+test_position_index()
