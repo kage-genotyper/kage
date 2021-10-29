4af5ade1f084aa510da54518a89ac8fc819f6e4e
Knut Rand <knutdrand@gmail.com>
2021-10-29 13:27:24 +0200

    missing files

---
+++ kage/helper_index.py
@@ -0,0 +1,90 @@
+import logging
+import numpy as np
+
+MAIN = -1
+HELPER = -2
+M = MAIN
+H = HELPER
+
+
+def make_helper_model_from_genotype_matrix(genotype_matrix, most_similar_variant_lookup=False, dummy_count=1):
+    genotype_matrix = genotype_matrix.matrix.transpose()
+
+    # genotypes are 1, 2, 3 (0 for unknown, 1 for homo ref, 2 for homo alt and 3 for hetero), we want 0, 1, 2 for homo alt, hetero, homo ref
+    logging.info("Converting genotype matrix to format in helper model code")
+    logging.info("Genotype matrix before conversion: %s" % genotype_matrix)
+    # 0, 1 => 2
+    # 2 => 0
+    # 3 => 1
+    new_genotype_matrix = np.zeros_like(genotype_matrix)
+    new_genotype_matrix[np.where(genotype_matrix == 0)] = -1
+    new_genotype_matrix[np.where(genotype_matrix == 1)] = 0
+    new_genotype_matrix[np.where(genotype_matrix == 2)] = 2
+    new_genotype_matrix[np.where(genotype_matrix == 3)] = 1
+    genotype_matrix = new_genotype_matrix
+
+    logging.info("Finding best helper")
+    logging.info("Using genotype matrix %s" % genotype_matrix)
+
+    if most_similar_variant_lookup is not None:
+        logging.info("Making from most similar variant lookup")
+        helpers = most_similar_variant_lookup.lookup_array
+    else:
+        logging.info("Making raw from genotype matrix")
+        logging.info("Creating combined matrices")
+        combined = create_combined_matrices(genotype_matrix, args.window_size)
+        helpers = find_best_helper(combined, calc_likelihood)
+
+    helper_counts = genotype_matrix[helpers] * 3
+    flat_idx = genotype_matrix + helper_counts
+    genotype_combo_matrix = np.array([(flat_idx == k).sum(axis=1) for k in range(9)]).T.reshape(-1, 3, 3) + dummy_count
+
+    return helpers, genotype_combo_matrix
+
+
+def create_combined_matrices(genotype_matrix, window_size):
+    logging.info("Multiplying genotype matrix")
+    helper = genotype_matrix*3
+    result = []
+    for i in range (1, window_size):
+        logging.info("Window size: %d/%d" % (i, window_size))
+        flat_idx = genotype_matrix[i:]+helper[:-i]
+        tmp = np.array([(flat_idx==k).sum(axis=1) for k in range(9)])
+        tmp = (tmp.T).reshape(-1, 3, 3)
+        result.append(tmp)
+    result = result
+    """variants, helper-g, main-g"""
+    return result
+
+def calc_likelihood(count_matrix):
+    """
+    Axis -2 is helper genotype
+    Axis -1 is main genotype
+    """
+    count_matrix = count_matrix+1
+    p = count_matrix/count_matrix.sum(axis=M, keepdims=True)
+    return np.sum(count_matrix*np.log(p), axis=(M, H))
+    
+def calc_argmax(count_matrix):
+    """
+    Axis -2 is helper genotype
+    Axis -1 is main genotype
+    """
+    return np.sum(np.max(count_matrix, axis=M), axis=-1)/count_matrix.sum(axis=(M, H))
+
+
+def find_best_helper(combined, score_func):
+    N = len(combined[0])+1
+    best_idx, best_score = np.empty(N, dtype="int"), -np.inf*np.ones(N)
+    for j, counts in enumerate(combined, 1):
+        scores = score_func(counts)
+        do_update = scores>best_score[j:]
+        best_score[j:][do_update] = scores[do_update]
+        best_idx[j:][do_update] = np.flatnonzero(do_update)
+        rev_scores = score_func(counts.swapaxes(-2, -1))
+        do_update = rev_scores>best_score[:-j]
+        best_score[:-j][do_update] = rev_scores[do_update]
+        best_idx[:-j][do_update] = np.flatnonzero(do_update)+j
+    ### best_idx[i] is the variant that helps predict variant i the best
+    return best_idx
+
---
+++ kage/models.py
@@ -0,0 +1,48 @@
+import numpy as np
+from scipy.special import logsumexp
+
+MAIN = -1
+HELPER = -2
+M = MAIN
+H = HELPER
+
+class Model:
+    def predict(self, k1, k2):
+        return np.argmax(self.score(k1, k2), axis=-1)
+
+    def score(self, k1, k2):
+        return np.array([self.logpmf(k1, k2, g) for g in [0, 1, 2]]).T
+
+    def logpmf(self, k1, k2, genotype):
+        raise NotImplemented
+
+class ComboModelBothAlleles(Model):
+    def __init__(self, model_ref, model_alt):
+        self._model_ref = model_ref
+        self._model_alt = model_alt
+
+    def logpmf(self, k1, k2, genotype):
+        ref_probs = self._model_ref.logpmf(k1, 2-genotype)
+        alt_probs = self._model_alt.logpmf(k2, genotype)
+        return ref_probs+alt_probs
+
+
+class HelperModel(Model):
+    def __init__(self, model, helper_variants, genotype_combo_matrix, tricky_variants=None):
+        self._model = model
+        self._helper_variants = helper_variants
+        self._genotype_probs = np.log(genotype_combo_matrix/genotype_combo_matrix.sum(axis=(-1, -2), keepdims=True))
+        self._tricky_variants = tricky_variants
+
+    def score(self, k1, k2):
+        count_probs = np.array([self._model.logpmf(k1, k2, g) for g in [0, 1, 2]]).T
+        log_probs =  self._genotype_probs + count_probs[self._helper_variants].reshape(-1, 3, 1)+count_probs.reshape(-1, 1, 3)
+        return logsumexp(log_probs, axis=H)
+
+    def logpmf(self, ref_counts, alt_counts, genotype):
+        count_probs = np.array([self._model.logpmf(ref_counts, alt_counts, g) for g in [0, 1, 2]]).T
+        if self._tricky_variants is not None:
+            logging.info("Using tricky variants")
+            count_probs = np.where(self._tricky_variants.reshape(-1, 1), np.log(1/3), count_probs)
+        log_probs =  self._genotype_probs+count_probs[self._helper_variants].reshape(-1, 3, 1)+count_probs.reshape(-1, 1, 3)
+        return logsumexp(log_probs, axis=H)[..., genotype]
