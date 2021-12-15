58f3e46a15b4b179b701f6aa392a78c4ae84cfa8
Ivar Grytten <ivar.grytten@gmail.com>
2021-12-15 23:05:13 +0100

    Obgraph requirement change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name='kage-genotyper',
-      version='0.0.14',
+      version='0.0.15',
       description='KAGE',
       long_description_content_type="text/markdown",
       url='http://github.com/ivargr/kage',
@@ -11,7 +11,7 @@ setup(name='kage-genotyper',
       packages=["kage"],
       zip_safe=False,
       install_requires=['numpy', 'tqdm', 'pyfaidx', 'pathos', 'cython', 'scipy',
-                        'obgraph',
+                        'obgraph>=0.0.8',
                         'graph_kmer_index>=0.0.11',
                         'kmer_mapper',
                         'graph_read_simulator', 'shared_memory_wrapper'
