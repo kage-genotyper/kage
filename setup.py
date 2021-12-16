e00a496f466d8b0436a1229fb656ddd1b7734bcb
Ivar Grytten <ivar.grytten@gmail.com>
2021-12-16 18:29:28 +0100

    Updated zenodo url

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name='kage-genotyper',
-      version='0.0.15',
+      version='0.0.16',
       description='KAGE',
       long_description_content_type="text/markdown",
       url='http://github.com/ivargr/kage',
@@ -12,7 +12,7 @@ setup(name='kage-genotyper',
       zip_safe=False,
       install_requires=['numpy', 'tqdm', 'pyfaidx', 'pathos', 'cython', 'scipy',
                         'obgraph>=0.0.8',
-                        'graph_kmer_index>=0.0.11',
+                        'graph_kmer_index>=0.0.12',
                         'kmer_mapper',
                         'graph_read_simulator', 'shared_memory_wrapper'
                         ],
