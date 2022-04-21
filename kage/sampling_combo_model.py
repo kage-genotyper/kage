aaf7fca4cee7e4588e2dcd1ed4701ab0e6c69e32
Knut Rand <knutdrand@gmail.com>
2022-04-21 11:59:06 +0200

    Sampling model testing

---
+++ kage/sampling_combo_model.py
@@ -0,0 +1,20 @@
+import numpy as np
+from dataclasses import dataclass
+
+
+@dataclass
+class SimpleSamplingComboModel:
+    expected: np.ndarray # N_nodes x 3
+
+
+    @classmethod
+    def from_counts(cls, base_lambda, counts_having_nodes, counts_not_having_nodes):
+        means_with_node = counts_having_nodes.mean(axis=-1)
+        means_without_node = counts_not_having_nodes.mean(axis=-1)
+        expected_0 = 2*means_without_node
+        expected_1 = means_without_node+means_with_node
+        expected_2 = 2*means_with_node
+        return cls(np.array([expected_0, expected_1, expected_2]).T*base_lambda)
+
+    def __eq__(self, other):
+        return np.all(self.expected, other.expected)
---
+++ tests/test_samplemodel.py
@@ -0,0 +1,42 @@
+import pytest
+import numpy as np
+from npstructures import RaggedArray
+
+from kage.sampling_combo_model import SimpleSamplingComboModel
+
+n_haplotypes = 4
+
+
+@pytest.fixture
+def counts_having_nodes():
+    return RaggedArray([1, 1, 2],
+                       [3],)
+
+@pytest.fixture
+def counts_not_having_nodes():
+    return RaggedArray([0],
+                       [0, 0, 1])
+
+@pytest.fixture
+def observed_counts():
+    return np.array([1, 0])
+
+
+@pytest.fixture
+def simple_sampling_combo_model():
+    return SimpleSamplingComboModel(
+        np.array([[0, 4/3, 8/3],
+                  [2/3, 1/3+3, 3*2]]))
+
+
+
+def test_simple_from_counts(counts_having_nodes, counts_not_having_nodes, 
+                            simple_sampling_combo_model):
+    assert SimpleSamplingComboModel.from_counts(
+        1, counts_having_nodes, counts_not_having_nodes) == simple_sampling_combo_model
+
+
+@pytest.mark.skip("unimplenented")
+def test_combomodel(sampling_combo_model, observed_counts):
+    score = sampling_combo_model.logpmf(observed_counts)
+    assert np.all(score == None)
