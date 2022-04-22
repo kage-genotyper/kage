f871790b3cbd5515bc658e76758c959c0cd03f0e
Knut Rand <knutdrand@gmail.com>
2022-04-22 11:54:39 +0200

    from counts in raggedfrequenecesamplingmodel

--- kage/sampling_combo_model.py
+++ kage/sampling_combo_model.py
@@ -1,6 +1,6 @@
 import numpy as np
 from scipy.stats import poisson
-
+from npstructures import RaggedArray
 from dataclasses import dataclass
 
 
@@ -22,3 +22,33 @@ class SimpleSamplingComboModel:
 
     def __eq__(self, other):
         return np.all(self.expected == other.expected)
+
+
+@dataclass
+class RaggedFrequencySamplingComboModel:
+    having_nodes_kmer_haplo_tuple: (RaggedArray, RaggedArray)
+    not_having_nodes_kmer_haplo_tuple: (RaggedArray, RaggedArray)
+
+    @classmethod
+    def _get_kmer_haplo_tuple(cls, counts_ragged_array, base_lambda):
+        kmer_counts = []
+        haplo_counts = []
+        # kmer_counts, haplo_counts = zip(*(np.unique(row, return_counts=True) for row in counts_ragged_array))
+        for row in counts_ragged_array:
+            unique, counts = np.unique(row, return_counts=True)
+            kmer_counts.append(unique)
+            haplo_counts.append(counts)
+
+        return (RaggedArray(kmer_counts)*base_lambda, RaggedArray(haplo_counts))
+
+    @classmethod
+    def from_counts(cls, base_lambda, counts_having_nodes, counts_not_having_nodes):
+        return cls(cls._get_kmer_haplo_tuple(counts_having_nodes, base_lambda),
+                   cls._get_kmer_haplo_tuple(counts_not_having_nodes, base_lambda))
+
+    def __eq__(self, other):
+        t = True
+        for i in range(2):
+            t &= np.all(self.having_nodes_kmer_haplo_tuple[i]==other.having_nodes_kmer_haplo_tuple[i])
+            t &= np.all(self.not_having_nodes_kmer_haplo_tuple[i]==other.not_having_nodes_kmer_haplo_tuple[i])
+        return t
--- tests/test_samplemodel.py
+++ tests/test_samplemodel.py
@@ -2,7 +2,7 @@ import pytest
 import numpy as np
 from npstructures import RaggedArray
 
-from kage.sampling_combo_model import SimpleSamplingComboModel
+from kage.sampling_combo_model import SimpleSamplingComboModel, RaggedFrequencySamplingComboModel
 from scipy.stats import poisson
 
 n_haplotypes = 4
@@ -30,6 +30,13 @@ def simple_sampling_combo_model():
                   [2/3, 1/3+3, 3*2]]))
 
 
+@pytest.fixture
+def ragged_frequency_sampling_combo_model():
+    return RaggedFrequencySamplingComboModel(
+        (RaggedArray([[1, 2], [3]]), RaggedArray([[2, 1], [1]])),
+        (RaggedArray([[0], [0, 1]]), RaggedArray([[1], [2, 1]])))
+
+
 def test_simple_from_counts(counts_having_nodes, counts_not_having_nodes, 
                             simple_sampling_combo_model):
     assert SimpleSamplingComboModel.from_counts(
@@ -44,3 +51,10 @@ def test_combomodel_logpmf(simple_sampling_combo_model, observed_counts):
              zip(observed_counts, simple_sampling_combo_model.expected)]
 
     assert np.all(score == truth)
+
+
+def test_frequency_from_counts(counts_having_nodes, counts_not_having_nodes,
+                               ragged_frequency_sampling_combo_model):
+
+    assert RaggedFrequencySamplingComboModel.from_counts(
+        1, counts_having_nodes, counts_not_having_nodes) == ragged_frequency_sampling_combo_model
