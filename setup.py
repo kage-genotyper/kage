9f684147a22fc352221eaa352941c053379d74f1
Ivar Grytten <ivar.grytten@gmail.com>
2022-09-09 17:28:41 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name='kage-genotyper',
-      version='0.1.0',
+      version='0.1.1',
       description='KAGE',
       long_description_content_type="text/markdown",
       url='http://github.com/ivargr/kage',
