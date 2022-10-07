e19e6724d41b34a095d283f866211d9a73923329
Ivar Grytten <ivar.grytten@gmail.com>
2022-10-07 17:25:52 +0200

    Changed requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name='kage-genotyper',
-      version='0.1.2',
+      version='0.1.3',
       description='KAGE',
       long_description_content_type="text/markdown",
       url='http://github.com/ivargr/kage',
@@ -11,12 +11,12 @@ setup(name='kage-genotyper',
       packages=["kage"],
       zip_safe=False,
       install_requires=['numpy', 'tqdm', 'pyfaidx', 'pathos', 'cython', 'scipy',
-                        'obgraph>=0.0.26',
+                        'obgraph>=0.0.27',
                         'graph_kmer_index>=0.0.20',
                         'kmer_mapper>=0.0.21',
-                        'graph_read_simulator>=0.0.6',
+                        'graph_read_simulator>=0.0.7',
                         'shared_memory_wrapper>=0.0.18',
-                        'bionumpy>=0.1.0', 'npstructures'
+                        'bionumpy>=0.2.0'
                         ],
       include_dirs=["."],
       classifiers=[
