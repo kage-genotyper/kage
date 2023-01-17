e64fc4013a7c5362b06d827a5479213faa244699
Ivar Grytten <ivar.grytten@gmail.com>
2023-01-17 10:41:41 +0100

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 
 setup(name='kage-genotyper',
-      version='0.1.9',
+      version='0.1.10',
       description='KAGE',
       long_description_content_type="text/markdown",
       url='http://github.com/ivargr/kage',
@@ -15,7 +15,7 @@ setup(name='kage-genotyper',
                         'graph_kmer_index>=0.0.22',
                         'kmer_mapper>=0.0.29',
                         'graph_read_simulator>=0.0.7',
-                        'shared_memory_wrapper>=0.0.24',
+                        'shared_memory_wrapper>=0.0.25',
                         'bionumpy>=0.2.15',
                         'npstructures>=0.2.9'
                         ],
