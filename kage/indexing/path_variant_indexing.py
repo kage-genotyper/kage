351b623d0772a54ca0da449e23a8516d2970da04
Ivar Grytten <ivar.grytten@gmail.com>
2023-05-29 17:17:04 +0200

    working setup for path variatn indexing

--- kage/indexing/path_variant_indexing.py
+++ kage/indexing/path_variant_indexing.py
@@ -1,18 +1,18 @@
 import itertools
 from dataclasses import dataclass
 from typing import List
-
+import npstructures as nps
 import numpy as np
 import numpy.typing as npt
 import bionumpy as bnp
 from bionumpy.bnpdataclass import bnpdataclass
-
+import graph_kmer_index
+from obgraph.variant_to_nodes import VariantToNodes
 
 """
 Module for simple variant signature finding by using static predetermined paths through the "graph".
 
 Works when all variants are biallelic and there are no overlapping variants.
-
 """
 
 
@@ -22,12 +22,69 @@ class GenomeBetweenVariants:
     sequence: bnp.EncodedRaggedArray
 
 
-@dataclass
 class Variants:
-    allele_sequences: List[bnp.EncodedRaggedArray]  # one EncodedRaggedArray for each allele
+    def __init__(self, data: bnp.EncodedRaggedArray, n_alleles: int = 2):
+        self._data = data  # data contains sequence for first allele first, then second allele, etc.
+        self.n_alleles = n_alleles
+        self.n_variants = len(self._data) // n_alleles
+
+    @classmethod
+    def from_list(cls, variant_sequences: List[List]):
+        zipped = list(itertools.chain(*zip(*variant_sequences)))
+        return cls(bnp.as_encoded_array(zipped, bnp.DNAEncoding))
+
+    @property
+    def allele_sequences(self):
+        return [self._data[self.n_variants*allele:self.n_variants*(allele+1)] for allele in range(self.n_alleles)]
+
+    def get_allele_sequence(self, variant, allele):
+        return self._data[self.n_variants*allele + variant].to_string()
+
+    def get_haplotype_sequence(self, haplotypes: np.ndarray) -> bnp.EncodedRaggedArray:
+        """
+        Gets all sequences from nodes given the haplotypes at those nodes.
+        """
+        assert len(haplotypes) == self.n_variants
+        rows = self.n_variants*haplotypes + np.arange(self.n_variants)
+        return self._data[rows]
+
+
+def zip_sequences(a: bnp.EncodedRaggedArray, b: bnp.EncodedRaggedArray):
+    """Utility function for merging encoded ragged arrays ("zipping" rows)"""
+    assert len(a) == len(b)+1
+
+    row_lengths = np.zeros(len(a)+len(b))
+    row_lengths[0::2] = a.shape[1]
+    row_lengths[1::2] = b.shape[1]
+
+    # empty placeholder to fill
+
+    new = bnp.EncodedRaggedArray(
+        bnp.EncodedArray(np.zeros(int(np.sum(row_lengths)), dtype=np.uint8), bnp.DNAEncoding),
+        row_lengths)
+    new[0::2] = a
+    new[1:-1:2] = b
+    return new
+
+
+@dataclass
+class Graph:
+    genome: GenomeBetweenVariants
+    variants: Variants
 
     def n_variants(self):
-        return len(self.allele_sequences[0])
+        return self.variants.n_variants
+
+    def sequence(self, haplotypes: np.ndarray) -> bnp.EncodedArray:
+        """
+        Returns the sequence through the graph given haplotypes for all variants
+        """
+        assert len(haplotypes) == self.n_variants()
+        ref_sequence = self.genome.sequence
+        variant_sequences = self.variants.get_haplotype_sequence(haplotypes)
+
+        # stitch these together
+        return zip_sequences(ref_sequence, variant_sequences)
 
 @dataclass
 class Paths:
@@ -35,6 +92,9 @@ class Paths:
     # the allele present at each variant in each path
     variant_alleles: np.ndarray
     #path_kmers: List[bnp.EncodedRaggedArray] = None
+
+    def n_variants(self):
+        return self.variant_alleles.shape[1]
 
     def paths_for_allele_at_variant(self, allele, variant):
         relevant_paths = np.where(self.variant_alleles[:, variant] == allele)[0]
@@ -57,24 +117,25 @@ class Paths:
 
 
 class PathCreator:
-    def __init__(self, variants: Variants, genome: GenomeBetweenVariants, window: int = 3):
-        self._variants = variants
-        self._genome = genome
-        assert len(genome.sequence) == self._variants.n_variants() + 1
+    def __init__(self, graph, window: int = 3):
+        self._graph = graph
+        self._variants = graph.variants
+        self._genome = graph.genome
         self._window = window
 
     def run(self):
         alleles = [0, 1]  # possible alleles, assuming biallelic variants
         n_paths = len(alleles)**self._window
-        n_variants = self._variants.n_variants()
+        n_variants = self._variants.n_variants
         combinations = self._make_combination_matrix(alleles, n_paths, n_variants)
 
         paths = []
 
         # each path will have sequences between each variant and the allele sequences specified in combinations
         ref_between = self._genome.sequence
-        for path, alleles in enumerate(combinations):
+        for i, alleles in enumerate(combinations):
             # make a new EncodedRaggedArray where every other row is ref/variant
+            """"
             n_rows = len(ref_between) + n_variants
             row_lengths = np.zeros(n_rows)
             row_lengths[0::2] = ref_between.shape[1]
@@ -89,6 +150,8 @@ class PathCreator:
             path = bnp.EncodedRaggedArray(bnp.EncodedArray(np.zeros(int(np.sum(row_lengths)), dtype=np.uint8), bnp.DNAEncoding), row_lengths)
             path[0::2] = ref_between
             path[1:-1:2] = variant_sequences
+            """
+            path = self._graph.sequence(alleles)
             paths.append(path)
 
         return Paths(paths, combinations)
@@ -109,3 +172,142 @@ class PathCreator:
         return combination_matrix
 
 
+class Scorer:
+    def __init__(self):
+        pass
+
+    def score_kmers(self, kmers):
+        pass
+
+
+class KmerFrequencyScorer(Scorer):
+    def __init__(self, frequency_lookup: nps.HashTable):
+        self._frequency_lookup = frequency_lookup
+
+    def score_kmers(self, kmers):
+        return np.max(self._frequency_lookup[kmers])
+
+
+
+@dataclass
+class Signatures:
+    ref: nps.RaggedArray
+    alt: nps.RaggedArray
+
+
+class SignatureFinder:
+    """
+    For each variant chooses a signature (a set of kmers) based one a Scores
+    """
+    def __init__(self, paths: Paths, scorer: Scorer, k=3):
+        self._paths = paths
+        self._scorer = scorer
+        self._k = k
+
+    def get_as_flat_kmers(self):
+        signatures = self.run()
+
+        all_kmers = []
+        all_node_ids = []
+
+        for variant_id, (ref_kmers, variant_kmers) in enumerate(zip(signatures.ref, signatures.alt)):
+            node_id_ref = variant_id * 2
+            node_id_alt = variant_id * 2 + 1
+            all_kmers.append(ref_kmers)
+            all_node_ids.append(np.full(len(ref_kmers), node_id_ref, dtype=np.uint32))
+            all_kmers.append(variant_kmers)
+            all_node_ids.append(np.full(len(variant_kmers), node_id_alt, dtype=np.uint32))
+
+        from graph_kmer_index import FlatKmers
+        return FlatKmers(np.array(all_kmers), np.array(all_node_ids))
+
+    def run(self) -> Signatures:
+        """
+        Returns a list of kmers. List contains alleles, each RaggedArray represents the variants
+        """
+        chosen_ref_kmers = []
+        chosen_alt_kmers = []
+        encoding = None
+
+        for variant in range(self._paths.n_variants()):
+            best_kmers = None
+            best_score = -1000000000
+            all_ref_kmers = self._paths.get_kmers(variant, 0, self._k)
+            all_alt_kmers = self._paths.get_kmers(variant, 1, self._k)
+            encoding = all_ref_kmers.encoding
+
+            assert np.all(all_ref_kmers.shape[1] == all_alt_kmers.shape[1])
+
+            for window in range(len(all_ref_kmers[0])):
+                ref_kmers = np.unique(all_ref_kmers[:, window].raw())
+                alt_kmers = np.unique(all_alt_kmers[:, window].raw())
+
+                all_kmers = np.concatenate([ref_kmers, alt_kmers])
+                window_score = self._scorer.score_kmers(all_kmers)
+
+                # if overlap between ref and alt, lower score
+                if len(set(ref_kmers).intersection(alt_kmers)) > 0:
+                    window_score -= 1000
+
+                if window_score > best_score:
+                    best_kmers = [ref_kmers, alt_kmers]
+                    best_score = window_score
+
+            chosen_ref_kmers.append(best_kmers[0])
+            chosen_alt_kmers.append(best_kmers[1])
+
+        return Signatures(nps.RaggedArray(chosen_ref_kmers), nps.RaggedArray(chosen_alt_kmers))
+
+
+class SignaturesWithNodes:
+    """
+    Represents signatures compatible with graph-nodes.
+    Nodes are given implicitly from variant ids. Makes it possible
+    to create a kmer index that requires nodes.
+    """
+    pass
+
+
+class SimpleKmerIndex:
+    # Lookup from kmer to variants and alleles
+    def __init__(self, lookup):
+        self._lookup = lookup
+
+    def get(self, kmers):
+        node_ids = lookup[kmers]
+        variant_ids = node_ids // 2
+        alleles = node_ids % 2
+        return variant_ids, alleles
+
+    def get_allele_counts(self, kmers, n_variants, n_alleles=2):
+        # returns a matrix of counts over variants. Rows are alleles
+        counts = np.zeros((n_alleles, n_variants), dtype=np.uint16)
+        variants, alleles = self.get(kmers)
+        for variant, allele in zip(variants, alleles):
+            counts[allele, variant] += 1
+
+        return counts
+
+    @classmethod
+    def from_signatures(cls, signatures: Signatures, modulo=None):
+        all_kmers = []
+        all_node_ids = []
+
+        for variant_id, (ref_kmers, variant_kmers) in enumerate(zip(signatures.ref, signatures.alt)):
+            node_id_ref = variant_id * 2
+            node_id_alt = variant_id * 2 + 1
+            all_kmers.append(ref_kmers)
+            all_node_ids.append(np.full(len(ref_kmers), node_id_ref, dtype=np.uint32))
+            all_kmers.append(variant_kmers)
+            all_node_ids.append(np.full(len(variant_kmers), node_id_alt, dtype=np.uint32))
+
+        return nps.HashTable(np.concatenate(all_kemrs), np.concatenate(all_node_ids), modulo=modulo)
+
+
+
+
+def index(graph: Graph):
+    paths = Paths(graph, window=3)
+    variant_signatures = SignatureFinder(paths).get_as_flat_kmers()
+    kmer_index = graph_kmer_index.KmerIndex.from_flat_kmers(variant_signatures)
+    variant_to_nodes = VariantToNodes(np.arange(graph.n_variants())*2, np.arange(graph.n_variants())*2+1)
--- tests/test_path_variant_indexing.py
+++ tests/test_path_variant_indexing.py
@@ -1,26 +1,51 @@
 import pytest
-from kage.indexing.path_variant_indexing import Paths, Variants, PathCreator, GenomeBetweenVariants
+from kage.indexing.path_variant_indexing import Paths, Variants, PathCreator, GenomeBetweenVariants, SignatureFinder, zip_sequences, Graph
 import bionumpy as bnp
+import numpy as np
 
 
 @pytest.fixture
 def variants():
-    allele_sequences = [
-        bnp.as_encoded_array(["A", "A", "", "A", "A"], bnp.DNAEncoding),  # ref
-        bnp.as_encoded_array(["C", "C", "C", "C", "C"], bnp.DNAEncoding),  # alt
-    ]
-    return Variants(allele_sequences)
+    #allele_sequences = [
+    #    bnp.as_encoded_array(["A", "A", "", "A", "A"], bnp.DNAEncoding),  # ref
+    #    bnp.as_encoded_array(["C", "C", "C", "C", "C"], bnp.DNAEncoding),  # alt
+    #]
+    #return Variants(allele_sequences)
+    return Variants.from_list(
+        [["A", "C"], ["A", "C"], ["", "C"], ["A", "C"], ["A", "C"]]
+    )
 
 
 @pytest.fixture
 def genome():
     return GenomeBetweenVariants(bnp.as_encoded_array(["GGG", "GG", "GG", "GG", "", "GGG"], bnp.DNAEncoding))
 
+@pytest.fixture
+def graph(genome, variants):
+    return Graph(genome, variants)
 
-def test_create_path(genome, variants):
-    print(genome.sequence)
-    print(variants.allele_sequences)
-    creator = PathCreator(variants, genome)
+
+def tests_variants(variants):
+    assert variants.get_allele_sequence(0, 0) == "A"
+    assert variants.get_allele_sequence(0, 1) == "C"
+    assert variants.get_allele_sequence(2, 0) == ""
+
+
+def test_get_haplotype_sequence(variants):
+    seq = variants.get_haplotype_sequence(np.array([0, 0, 0, 0, 0])).tolist()
+    assert seq == ["A", "A", "", "A", "A"]
+
+    seq = variants.get_haplotype_sequence(np.array([0, 1, 1, 0, 1])).tolist()
+    assert seq == ["A", "C", "C", "A", "C"]
+
+
+def test_graph(genome, variants):
+    graph = Graph(genome, variants)
+    assert graph.sequence(np.array([0, 1, 1, 0, 1])).ravel().to_string() == "GGGAGGCGGCGGACGGG"
+
+
+def test_create_path(graph):
+    creator = PathCreator(graph)
     paths = creator.run()
     print(paths)
     assert len(paths.paths) == 8
@@ -29,4 +54,32 @@ def test_create_path(genome, variants):
 def test_get_kmers(genome, variants):
     creator = PathCreator(variants, genome)
     paths = creator.run()
-    print(paths)
+    kmers = paths.get_kmers(0, 0)
+
+
+class DummyScorer:
+    def __init__(self):
+        pass
+
+    def score_kmers(self, kmers):
+        return 1
+
+
+def test_signatures(graph):
+    creator = PathCreator(graph)
+    paths = creator.run()
+    signature_finder = SignatureFinder(paths, DummyScorer())
+    signatures = signature_finder.run()
+    print(signatures)
+
+
+def test_simple_kmer_index():
+    index = nps.HashTable()
+
+
+def test_zip_sequences():
+    a = bnp.as_encoded_array(["AA", "CC", "GG", "TT", "C"], bnp.DNAEncoding)
+    b = bnp.as_encoded_array(["T", "", "T", "AAA"], bnp.DNAEncoding)
+
+    correct = "AATCCGGTTTAAAC"
+    assert zip_sequences(a, b).ravel().to_string() == correct
